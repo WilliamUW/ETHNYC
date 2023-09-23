56ddcfeadc5800ebffc0002745c2819b1d744f49
Alejandro Gala <alegala21@gmail.com>
2023-09-23 16:49:07 -0400

    axelar base - untested

--- onchain/contracts/Reputable.sol
+++ onchain/contracts/Reputable.sol
@@ -6,14 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {AncillaryData} from "./uma/AncillaryData.sol";
 import {OptimisticOracleV3Interface} from "./uma/OptimisticOracleV3Interface.sol";
+import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import {IAxelarGateway} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
 import {OptimisticOracleV3CallbackRecipientInterface} from "./uma/OptimisticOracleV3CallbackRecipientInterface.sol";
 
-contract Reputable is ERC721, Ownable {
+contract Reputable is ERC721, Ownable, AxelarExecutable {
     using Strings for uint;
 
     string highRep;
     string medRep;
     string lowRep;
+
+    IAxelarGasService public immutable gasService;
 
     struct Investment {
         address investor;
@@ -30,40 +35,65 @@ contract Reputable is ERC721, Ownable {
         bytes32 assertionId;
     }
 
+    constructor(
+        string memory highRepURI,
+        string memory medRepURI,
+        string memory lowRepURI,
+        address _gateway,
+        address _gasReciever
+    ) AxelarExecutable(_gateway) {
+        gasService = IAxelarGasService(_gasReciever);
+        highRep = highRepURI;
+        medRep = medRepURI;
+        lowRep = lowRepURI;
+    }
+
     mapping(address => Fund[]) public investors;
     mapping(uint16 => Fund) public funds;
     mapping(uint16 => Investment[]) public investorsInFund;
+    mapping(string => uint) public estimatedGasAmount;
+
+    string[] destinationChains;
+    string[] addys;
 
     string private _name;
     string private _symbol;
     string private _baseURI;
 
+    uint private nonce;
+
     uint256 public constant MAX_REPUTATION = 100;
     uint16 public fundCount;
 
     OptimisticOracleV3Interface private _disputeOracle;
+    mapping(uint256 => bool) public executed;
+    mapping(uint256 => bytes32) public destination;
 
     modifier onlyFunds(uint16 _id) {
         require(funds[_id].eoa == msg.sender);
         _;
     }
 
-    function invest(
-        address _token,
-        uint256 _amount,
-        uint16 _id
-    ) public payable {
-        if (_ownerOf(_id) == address(0)) revert();
+    function setCrossChainConfig(
+        string[] memory _addys,
+        string[] memory _destinationChains
+    ) external onlyOwner {
+        destinationChains = _destinationChains;
+        addys = _addys;
+    }
 
+    function setInvestStatus(uint16 _id, bool status) external onlyOwner {
+        funds[_id].isOpen = status;
+    }
+
+    function invest(uint256 _amount, uint16 _id) public payable {
+        if (_ownerOf(_id) == address(0)) revert();
         Fund memory fund = funds[_id];
         require(fund.isOpen, "not accepting investment");
-        if (address(_token) != address(0)) {
-            IERC20(_token).transferFrom(msg.sender, fund.eoa, _amount);
-        } else {
-            require(fund.reputation > 0, "reputation too low");
-            require(msg.value == _amount, "send the correct amount");
-            fund.eoa.call{value: _amount}("");
-        }
+        require(fund.reputation > 0, "reputation too low");
+        require(msg.value == _amount, "send the correct amount");
+
+        fund.eoa.call{value: _amount}("");
 
         Fund[] memory investedFunds = investors[msg.sender];
         fund.totalRaised += _amount;
@@ -88,14 +118,17 @@ contract Reputable is ERC721, Ownable {
 
     function returnYield(uint16 _id) external payable onlyFunds(_id) {
         Fund memory fund = funds[_id];
+        require(!fund.isOpen);
         for (uint i = 0; i < investorsInFund[_id].length; ) {
             investorsInFund[_id][i].investor.call{
-                value: msg.value * investorsInFund[_id][i].sharePercentage
+                value: (msg.value * investorsInFund[_id][i].sharePercentage) /
+                    10000
             }("");
             unchecked {
                 i++;
             }
         }
+        funds[_id].totalRaised = 0;
     }
 
     function assertBadReputation(uint16 _id) public {
@@ -151,16 +184,24 @@ contract Reputable is ERC721, Ownable {
         }
         if (assertedTruthfully) {
             funds[id].reputation -= 10;
+            for (uint i = 0; i < destinationChains.length; i++) {
+                setCrossChainReputation(
+                    destinationChains[i],
+                    addys[i],
+                    abi.encode(id, funds[id].reputation)
+                );
+            }
         }
         funds[id].assertionId = bytes32(0);
     }
 
+    // should be called on all chains
     function addFund(address _eoa, uint256 _repuation) public onlyOwner {
         Fund memory newFund = Fund(
             _repuation,
             0,
             fundCount,
-            false,
+            true,
             _eoa,
             bytes32(0)
         );
@@ -181,8 +222,8 @@ contract Reputable is ERC721, Ownable {
 
         for (uint16 i = 0; i < investorsInFund[_id].length; ) {
             investorsInFund[_id][i].sharePercentage =
-                fund.totalRaised /
-                investorsInFund[_id][i].amount;
+                (fund.totalRaised / investorsInFund[_id][i].amount) *
+                10000;
             unchecked {
                 i++;
             }
@@ -197,13 +238,92 @@ contract Reputable is ERC721, Ownable {
             if (funds[i].assertionId == assertionId) {
                 id = i;
             }
-
             unchecked {
                 i++;
             }
         }
 
         funds[id].assertionId = bytes32(0);
+    }
+
+    function adjustHighRepURI(string memory _newURI) external onlyOwner {
+        highRep = _newURI;
+    }
+
+    function adjustMedRepURI(string memory _newURI) external onlyOwner {
+        medRep = _newURI;
+    }
+
+    function adjustLowRepURI(string memory _newURI) external onlyOwner {
+        lowRep = _newURI;
+    }
+
+    function setCrossChainReputation(
+        string memory destinationChain,
+        string memory contractAddress,
+        bytes memory payload
+    ) internal {
+        uint256 _nonce = nonce;
+        bytes memory modifiedPayload = abi.encode(_nonce, payload);
+        gasService.payNativeGasForContractCall{
+            value: estimatedGasAmount[destinationChain]
+        }(
+            address(this),
+            destinationChain,
+            contractAddress,
+            modifiedPayload,
+            msg.sender
+        );
+
+        gateway.callContract(
+            destinationChain,
+            contractAddress,
+            modifiedPayload
+        );
+
+        destination[_nonce] = _getDestinationHash(
+            destinationChain,
+            contractAddress
+        );
+        nonce = _nonce + 1;
+    }
+
+    function adjustEstimatedGasAmount(
+        uint256 _newAmount,
+        string memory chain
+    ) public onlyOwner {
+        estimatedGasAmount[chain] = _newAmount;
+    }
+
+    function _execute(
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload
+    ) internal override {
+        uint256 _nonce = abi.decode(payload, (uint256));
+        (uint16 fundId, uint256 newRep) = abi.decode(
+            payload,
+            (uint16, uint256)
+        );
+        if (
+            destination[_nonce] !=
+            _getDestinationHash(sourceChain, sourceAddress)
+        ) {
+            return;
+        }
+        if (funds[fundId].eoa != address(0)) {
+            funds[fundId].reputation = newRep;
+        }
+
+        executed[_nonce] = true;
+        destination[_nonce] = 0;
+    }
+
+    function _getDestinationHash(
+        string memory destinationChain,
+        string memory contractAddress
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encode(destinationChain, contractAddress));
     }
 
     function _beforeTokenTransfer(
